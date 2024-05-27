bdfa210c36cbdd295481050fa44d382901989660
Leonardo Moreira Louzas <leonardomlouzas@gmail.com>
2024-05-27 15:54:37 -0300

    Adds Form and Dropdown

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,9 +1,11 @@
 import Banner from './componentes/Banner';
+import Form from './componentes/Form';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Form />
     </div>
   );
 }
---
+++ organo/src/componentes/Form/index.js
@@ -0,0 +1,30 @@
+import DropDown from "../DropDown";
+import TextField from "../TextField";
+import "./Form.css"
+
+
+const Form = () => {
+    const times = [
+        'Programação',
+        'Front-end',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    return (
+        <section className="form">
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <TextField label="Nome" placeholder="Digite seu nome" />
+                <TextField label="Cargo" placeholder="Digite seu cargo" />
+                <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
+                <DropDown label="Time" itens={times} />
+            </form>
+        </section>
+    )
+}
+
+export default Form;
